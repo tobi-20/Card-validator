4bc2014007620d290d5804dc214939297ad0c2bf
Tobi-20 <olutobiseun18@gmail.com>
2026-04-28 17:58:01 +0100

    prepare for deployment

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { cardHandler } from './card/handlers';
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 
 app.post('/validate-card', cardHandler.ValidateCard);
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
