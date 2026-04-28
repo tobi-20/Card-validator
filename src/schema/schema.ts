c30b3c3f7b8fa5919b3706517f4f9e9cfed3f8ea
tobi-20 <olutobiseun18@gmail.com>
2026-04-28 10:31:43 +0100

    add zod validation schema

---
+++ src/schema/schema.ts
@@ -0,0 +1,7 @@
+import z from 'zod';
+
+export const validateCardSchema = z.object({
+  cardNumber: z.string().min(13).max(19),
+});
+
+export type ValidateCardInput = z.infer<typeof validateCardSchema>;
